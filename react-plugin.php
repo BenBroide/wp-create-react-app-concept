cdbcb1aaf8ab9965c7aa75a94477b813e6d386e3
Be Broide <be.broide@gmail.com>
2020-05-25 05:32:51 -0400

    added class

--- react-plugin.php
+++ react-plugin.php
@@ -12,10 +12,14 @@
 
 
 // Setting react app path constants.
+define('RP_PLUGIN_VERSION','0.1.0' );
 define('RP_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) . 'react-app/');
 define('RP_REACT_APP_BUILD', RP_PLUGIN_DIR_URL . 'build/');
 define('RP_MANIFEST_URL', RP_REACT_APP_BUILD . 'asset-manifest.json');
 
+/**
+ *
+ */
 function rp_load_plugin(){
 	new RpLoadReactApp('admin_enqueue_scripts', 'index.php', false,'#wpbody .wrap');
 	new RpLoadReactApp( 'wp_enqueue_scripts', 'index.php', 'is_front_page', '#site-footer');
@@ -75,23 +79,9 @@ class RpLoadReactApp {
 		if(is_string($limit_callback) && !$limit_callback()  )
 			return;
 
-		// Request manifest file.
-		$request = file_get_contents( RP_MANIFEST_URL );
-
-		// If the remote request fails return.
-		if ( !$request  )
-			return false;
-
-		// Convert json to php array.
-		$files_data = json_decode( $request );
-		if ( $files_data === null )
-			return;
-
-		if ( ! property_exists( $files_data, 'entrypoints' ) )
-			return false;
 
 		// Get assets links.
-		$assets_files = $files_data->entrypoints;
+		$assets_files = $this->get_assets_files();
 
 		$js_files  = array_filter( $assets_files,  function($file_string) {
 			return pathinfo( $file_string, PATHINFO_EXTENSION ) === 'js';
@@ -108,12 +98,36 @@ class RpLoadReactApp {
 
 		// Load js files.
 		foreach ( $js_files as $index => $js_file ) {
-			wp_enqueue_script( 'react-plugin-' . $index, RP_REACT_APP_BUILD . $js_file, array(), 1, true );
+			wp_enqueue_script( 'react-plugin-' . $index, RP_REACT_APP_BUILD . $js_file, array(), RP_PLUGIN_VERSION, true );
 		}
 
-		// Variables for app use.
+		// Variables for app use - These variables will be available in window.rpReactPlugin variable.
 		wp_localize_script( 'react-plugin-0', 'rpReactPlugin',
 			array( 'appSelector' => $this->selector )
 		);
+	}
+
+	/**
+	 * Get app assets files.
+	 *
+	 * @return bool|void
+	 */
+	private function get_assets_files(){
+		// Request manifest file.
+		$request = file_get_contents( RP_MANIFEST_URL );
+
+		// If the remote request fails return.
+		if ( !$request  )
+			return false;
+
+		// Convert json to php array.
+		$files_data = json_decode( $request );
+		if ( $files_data === null )
+			return;
+
+		if ( ! property_exists( $files_data, 'entrypoints' ) )
+			return false;
+
+		return $files_data->entrypoints;
 	}
 }
