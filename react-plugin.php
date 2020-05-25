6c0596eac54f7b48fb8d32e224ab1f93d94f82ee
Be Broide <be.broide@gmail.com>
2020-05-25 05:25:53 -0400

    added class

--- react-plugin.php
+++ react-plugin.php
@@ -10,82 +10,110 @@
  * @package         React_Plugin
  */
 
-/**
- * Load react app files in WordPress admin.
- *
- * @return bool|void
- */
-function rp_load_react_app($hook){
 
-	$is_main_dashboard = $hook === 'index.php';
+// Setting react app path constants.
+define('RP_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) . 'react-app/');
+define('RP_REACT_APP_BUILD', RP_PLUGIN_DIR_URL . 'build/');
+define('RP_MANIFEST_URL', RP_REACT_APP_BUILD . 'asset-manifest.json');
 
-	// Only load react app scripts in main admin page.
-	if( !$is_main_dashboard)
-		return;
-
-	// Setting path variables.
-	$plugin_dir_url =plugin_dir_url( __FILE__ ) . 'react-app/';
-	$react_app_build = $plugin_dir_url .'build/';
-	$manifest_url = $react_app_build. 'asset-manifest.json';
-
-	// Request manifest file.
-	$request = file_get_contents( $manifest_url );
-
-	// If the remote request fails, wp_remote_get() will return a WP_Error, so let’s check if the $request variable is an error:
-	if( is_wp_error( $request ) )
-		return false;
-
-	// Convert json to php array.
-	$files_data = json_decode($request);
-	if($files_data === null)
-		return ;
-
-	if(!property_exists($files_data,'entrypoints'))
-		return false;
-
-	// Get assets links.
-	$assets_files = $files_data->entrypoints;
-
-	$js_files = array_filter($assets_files,'rp_filter_js_files');
-	$css_files = array_filter($assets_files,'rp_filter_css_files');
-
-	// Load css files
-	foreach ($css_files as $index => $css_file){
-		wp_enqueue_style('react-plugin-'.$index, $react_app_build . $css_file);
-	}
-
-	// Load js files
-	foreach ($js_files as $index => $js_file){
-		wp_enqueue_script('react-plugin-'.$index, $react_app_build . $js_file, array(), 1, true);
-	}
-
-	// Variables for app use
-	wp_localize_script('react-plugin-0', 'rpReactPlugin',
-		array('appSelector' => '#wpbody .wrap')
-	);
+function rp_load_plugin(){
+	new RpLoadReactApp('admin_enqueue_scripts', 'index.php', false,'#wpbody .wrap');
+	new RpLoadReactApp( 'wp_enqueue_scripts', 'index.php', 'is_front_page', '#site-footer');
 }
 
-// Setting hook to load files.
-add_action('admin_enqueue_scripts','rp_load_react_app');
+add_action('init','rp_load_plugin');
 
-/**
- * Get js files from assets array.
- *
- * @param array $file_string
- *
- * @return bool
- */
-function rp_filter_js_files ($file_string){
-  return pathinfo($file_string, PATHINFO_EXTENSION) === 'js';
-}
+class RpLoadReactApp {
 
-/**
- * Get css files from assets array.
- *
- * @param array $file_string
- *
- * @return bool
- */
-function rp_filter_css_files ($file_string) {
-	return pathinfo( $file_string, PATHINFO_EXTENSION ) === 'css';
+	/**
+	 * @var string
+	 */
+	private $selector = '';
+	/**
+	 * @var string
+	 */
+	private $limit_load_hook = '';
+	/**
+	 * @var bool|string
+	 */
+	private $limit_callback = '';
+
+	/**
+	 * RpLoadReactApp constructor.
+	 *
+	 * @param string $enqueue_hook Hook to enqueue scripts.
+	 * @param string $limit_load_hook Limit load to hook in admin load.
+	 * @param bool|string $limit_callback Limit load by callback result.
+	 * @param string $css_selector Css selector to render app.
+	 */
+	function __construct( $enqueue_hook, $limit_load_hook,$limit_callback = false, $css_selector)  {
+		$this->selector = $css_selector;
+		$this->limit_load_hook = $limit_load_hook;
+		$this->limit_callback= $limit_callback;
+
+		add_action( $enqueue_hook, [$this,'load_react_app']);
+	}
+
+	/**
+	 * Load react app files in WordPress admin.
+	 *
+	 * @param $hook
+	 *
+	 * @return bool|void
+	 */
+	function load_react_app( $hook ) {
+
+		$is_main_dashboard = $hook === $this->limit_load_hook;
+
+		// Limit app load in admin by admin page hook.
+		if ( ! $is_main_dashboard && is_bool($this->limit_callback))
+			return;
+
+		// Limit app load in front end by callback.
+		$limit_callback = $this->limit_callback;
+
+		if(is_string($limit_callback) && !$limit_callback()  )
+			return;
+
+		// Request manifest file.
+		$request = file_get_contents( RP_MANIFEST_URL );
+
+		// If the remote request fails return.
+		if ( !$request  )
+			return false;
+
+		// Convert json to php array.
+		$files_data = json_decode( $request );
+		if ( $files_data === null )
+			return;
+
+		if ( ! property_exists( $files_data, 'entrypoints' ) )
+			return false;
+
+		// Get assets links.
+		$assets_files = $files_data->entrypoints;
+
+		$js_files  = array_filter( $assets_files,  function($file_string) {
+			return pathinfo( $file_string, PATHINFO_EXTENSION ) === 'js';
+		});
+
+		$css_files = array_filter( $assets_files,  function($file_string) {
+			return pathinfo( $file_string, PATHINFO_EXTENSION ) === 'css';
+		});
+
+		// Load css files.
+		foreach ( $css_files as $index => $css_file ) {
+			wp_enqueue_style( 'react-plugin-' . $index, RP_REACT_APP_BUILD . $css_file );
+		}
+
+		// Load js files.
+		foreach ( $js_files as $index => $js_file ) {
+			wp_enqueue_script( 'react-plugin-' . $index, RP_REACT_APP_BUILD . $js_file, array(), 1, true );
+		}
+
+		// Variables for app use.
+		wp_localize_script( 'react-plugin-0', 'rpReactPlugin',
+			array( 'appSelector' => $this->selector )
+		);
+	}
 }
